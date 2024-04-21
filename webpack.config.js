034e935e94053d7413861de472b02c90421da21b
zeuspsy0x1 <zeusvargaswork@gmail.com>
2024-04-20 20:59:15 -0500

    remove tailwind, clean component

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,7 @@ module.exports = {
             ident: 'postcss',
             syntax: 'postcss-scss',
             plugins: () => [
-              require('postcss-import'),
-              require('tailwindcss'),
-              require('autoprefixer'),
+
             ]
           }
         }
